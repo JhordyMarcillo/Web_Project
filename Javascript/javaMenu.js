81a7139ce5f2b1ceef5dfacba45a03eae6ace3bd
JhordyMarcillo <jhordypaulsb2@gmail.com>
2024-08-14 00:09:31 -0500

    Update Menu, Folders and htmls

--- Javascript/javaMenu.js
+++ Javascript/javaMenu.js
@@ -11,6 +11,74 @@ function myAccFunc() {
 // Click on the "Jeans" link on page load to open the accordion for demo purposes
 document.getElementById("myBtn").click();
 
+function myAccFuncBebidas() {
+  var x = document.getElementById("demoAccBebidas");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+document.getElementById("myBtnBebidas").click();
+
+function myAccFuncLacteos() {
+  var x = document.getElementById("demoAccLacteos");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+document.getElementById("myBtnLacteos").click();
+
+function myAccFuncCongelados() {
+  var x = document.getElementById("demoAccCongelados");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+document.getElementById("myBtnCongelados").click();
+
+
+function myAccFuncCarnes() {
+  var x = document.getElementById("demoAccCarnes");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+document.getElementById("myBtnCarnes").click();
+
+
+function myAccFuncPanificadora() {
+  var x = document.getElementById("demoAccPanificadora");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+document.getElementById("myBtnPanificadora").click();
+
+
+function myAccFuncConfiteria() {
+  var x = document.getElementById("demoAccConfiteria");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+document.getElementById("myBtnConfiteria").click();
 
 // Open and close sidebar
 function w3_open() {
